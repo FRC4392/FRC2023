bf63634aa83c4daefd908ef87ef94a7b31d148c2
deceiversrobotics <122321750+deceiversrobotics@users.noreply.github.com>
2023-03-09 18:19:30 -0500

    removed demons

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,28 +4,18 @@
 
 package frc.robot;
 
-import java.util.function.BooleanSupplier;
-import java.util.function.DoubleSupplier;
-
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.commands.Autos;
+
 import frc.robot.commands.DriveCommand;
-import frc.robot.commands.resetArmEncoders;
-import frc.robot.subsystems.Arm;
+
 import frc.robot.subsystems.Drivetrain;
-import frc.robot.subsystems.Intake;
-import frc.robot.subsystems.JoeBident;
-import frc.robot.subsystems.LED;
-import frc.robot.subsystems.Limelight;
+
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -40,11 +30,7 @@ public class Robot extends TimedRobot {
   CommandXboxController operatorController = new CommandXboxController(1);
 
   Drivetrain drivetrain = new Drivetrain();
-  Arm arm = new Arm();
-  JoeBident bident = new JoeBident();
-  Intake intake = new Intake();
-  Limelight limelight = new Limelight();
-  LED led = new LED();
+
 
   SendableChooser<String> chooser = new SendableChooser<>();
 
@@ -84,42 +70,17 @@ public class Robot extends TimedRobot {
   /** This function is called once each time the robot enters Disabled mode. */
   @Override
   public void disabledInit() {
-    led.setMode(0);
   }
 
   @Override
   public void disabledPeriodic() {
-    if (DriverStation.getAlliance() == Alliance.Blue){
-    bident.setLEDColor(0, 0, 1);
-    } else {
-      bident.setLEDColor(1, 0, 0);
-    }
+
   }
 
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
-    switch(chooser.getSelected()){
-      case "LoadingStation":
-        m_autonomousCommand = Autos.getLoadingStationCommand(arm, bident, drivetrain, intake);
-      break;
-      case "Balance":
-        m_autonomousCommand = Autos.getBalanceCommand(arm, bident, drivetrain, intake);
-      break;
-      case "Bump":
-        m_autonomousCommand = Autos.getBumpCommand(arm, bident, drivetrain, intake);
-      break;
-      case "Stupid":
-        m_autonomousCommand = Autos.getStupidCommand(arm, bident, drivetrain, intake);
-      break;
-      default:
-        m_autonomousCommand = null;
-    }
-    // schedule the autonomous command (example)
-    led.setMode(1);
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
-    }
+
   }
 
   /** This function is called periodically during autonomous. */
@@ -134,8 +95,7 @@ public class Robot extends TimedRobot {
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
     // this line or comment it out.
-    led.setMode(2);
-    if (m_autonomousCommand != null) {
+  if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
   }
@@ -149,7 +109,6 @@ public class Robot extends TimedRobot {
   @Override
   public void testInit() {
     // Cancels all running commands at the start of test mode.
-    led.setMode(3);
     CommandScheduler.getInstance().cancelAll();
   }
 
@@ -162,7 +121,6 @@ public class Robot extends TimedRobot {
   /** This function is called once when the robot is first started up. */
   @Override
   public void simulationInit() {
-    led.setMode(4);
   }
 
   /** This function is called periodically whilst in simulation. */
@@ -172,59 +130,7 @@ public class Robot extends TimedRobot {
   }
 
   private void configureBindings() {
-    Trigger CubeTrigger = operatorController.povLeft();
-    Trigger highGoalConeTrigger = operatorController.y().and(CubeTrigger.negate());
-    Trigger highGoalCubeTrigger = operatorController.y().and(CubeTrigger);
-    Trigger MidGoalConeTrigger = operatorController.x().and(CubeTrigger.negate());
-    Trigger MidGoalCubeTrigger = operatorController.x().and(CubeTrigger);
-    Trigger shelfTrigger = operatorController.b().and(CubeTrigger.negate());
-    Trigger lowGoalConeTrigger = operatorController.a().and(CubeTrigger.negate());
-    Trigger lowGoalCubeTrigger = operatorController.a().and(CubeTrigger);
 
-    Trigger groundIntake = operatorController.leftStick().and(CubeTrigger.negate());;
-    Trigger groundOuttake = operatorController.leftStick().and(CubeTrigger);
-
-    Trigger cubeIndicatorTrigger = operatorController.povDown();
-    Trigger coneIndicatorTrigger = operatorController.povUp();
-
-    BooleanSupplier brakeSupplier = () -> driverController.getXButton();
-    BooleanSupplier gripperSupplier = () -> bident.getGripperOccupied();
-
-    Trigger brake = new Trigger(brakeSupplier);
-    Trigger gripperReady = new Trigger(gripperSupplier);
-
-   // brake.whileTrue(drivetrain.brakeCommand());
-    gripperReady.whileTrue(led.accyGreem());
-
-    operatorController.rightStick().onTrue(arm.shoulderPositionCommand(0).andThen(arm.elbowPositionCommand(0)).andThen(intake.getIntakePivotCommand(0.0)));
-    highGoalConeTrigger.onTrue(arm.elbowPositionCommand(-140.0).andThen(arm.shoulderPositionCommand(-37.0)));
-    highGoalCubeTrigger.onTrue(arm.elbowPositionCommand(-110).andThen(arm.shoulderPositionCommand(-17)));
-    MidGoalConeTrigger.onTrue(arm.elbowPositionCommand(-110).andThen(arm.shoulderPositionCommand(-8)));
-    MidGoalCubeTrigger.onTrue(arm.elbowPositionCommand(-85).andThen(arm.shoulderPositionCommand(0)));
-    lowGoalConeTrigger.onTrue(arm.elbowPositionCommand(-20).andThen(arm.shoulderPositionCommand(-20.0)));
-    lowGoalCubeTrigger.onTrue(arm.elbowPositionCommand(-20).andThen(arm.shoulderPositionCommand(-20.0)));
-
-    shelfTrigger.onTrue(arm.elbowPositionCommand(-108).andThen(arm.shoulderPositionCommand(12.0)));
-
-     cubeIndicatorTrigger.whileTrue(bident.cubeCommand());
-     coneIndicatorTrigger.whileTrue(bident.coneCommand());
-
-     operatorController.back().whileTrue(arm.resetEncoder());
-
-    DoubleSupplier intakeSpeed = () -> operatorController.getLeftTriggerAxis() - operatorController.getRightTriggerAxis();
-      
-    // bident.setDefaultCommand(new manualGripper3(bident, intakeSpeed));
-
-    operatorController.leftTrigger(0).whileTrue(bident.autoGrabCommand(intakeSpeed).alongWith(intake.getIntakeCommand()));
-    operatorController.rightTrigger(0).whileTrue(bident.ejectWithoutOpeningCommand(intakeSpeed));
-
-    groundIntake.whileTrue(intake.getIntakePivotCommand(95.0).andThen(arm.elbowPositionCommand(15
-    ).andThen(arm.shoulderPositionCommand(17).alongWith(bident.autoIntakeCommand(0.5).alongWith(intake.getIntakeCommand())))));
-    groundIntake.onFalse(arm.shoulderPositionCommand(0).andThen(arm.elbowPositionCommand(0)).andThen(intake.getIntakePivotCommand(0.0)));
-    groundOuttake.whileTrue(intake.getIntakePivotCommand(95.0).andThen(arm.elbowPositionCommand(18).andThen(arm.shoulderPositionCommand(15).alongWith(intake.getOuttakeCommand()))));
-
-    operatorController.leftBumper().whileTrue(bident.closeCommand());
-    operatorController.rightBumper().whileTrue(bident.openCommand());
     drivetrain.setDefaultCommand(new DriveCommand(drivetrain, driverController));
   }
 }
